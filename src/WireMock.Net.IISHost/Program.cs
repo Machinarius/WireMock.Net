ed3a4b4926144e258ec7ebd79c96e2da05728439
Germán Valencia <machinarius92@gmail.com>
2017-05-08 19:22:08 -0500

    IIS host is working on IIS Express, local IIS and Azure App service

--- src/WireMock.Net.IISHost/Program.cs
+++ src/WireMock.Net.IISHost/Program.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Logging;
-using WireMock.Owin;
-using WireMock.Server;
 
 namespace WireMock.Net.IISHost
 {
@@ -10,15 +8,8 @@ namespace WireMock.Net.IISHost
     {
         public static void Main(string[] args)
         {
-            var options = new WireMockMiddlewareOptions();
-            var server = FluentMockServer.StartWithHostedMiddlewareOptions(options, true, false);
-
             var host = new WebHostBuilder()
                 .ConfigureLogging(factory => factory.AddConsole(LogLevel.Information))
-                .Configure(appBuilder =>
-                {
-                    appBuilder.UseMiddleware<WireMockMiddleware>(options);
-                })
                 .UseKestrel()
                 .UseIISIntegration()
                 .UseStartup<Startup>()
--- src/WireMock.Net.IISHost/Startup.cs
+++ src/WireMock.Net.IISHost/Startup.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using WireMock.Owin;
+using WireMock.Server;
 
 namespace WireMock.Net.IISHost
 {
@@ -28,12 +25,9 @@ namespace WireMock.Net.IISHost
                 app.UseDeveloperExceptionPage();
             }
 
-            /*
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
-            */
+            var options = new WireMockMiddlewareOptions();
+            var server = FluentMockServer.StartWithHostedMiddlewareOptions(options, true, false);
+            app.UseMiddleware<WireMockMiddleware>(options);
         }
     }
 }
